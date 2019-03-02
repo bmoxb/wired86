214bc1b4781424c2d6cb4f34985918eba9bdea78
WiredSound <maxoblack@yahoo.com>
2019-03-02 20:05:38 +0000

    Added documentation to logger.

--- include/common/logging.hpp
+++ include/common/logging.hpp
@@ -17,28 +17,55 @@
 #endif
 
 namespace logging {
+    /**
+     * Structure holding information about a call to a logging method. Holds the line number, file and function name
+     * from which a log call is made. Should not be made manually - use the ADDITIONAL_LOGGING_INFO macro instead as a
+     * second argument to a logging method in Logger.
+     */
     struct LoggingInfo {
         unsigned long line;
         std::string file;
         std::string function;
     };
 
+    /**
+     * Allows for the logging of information, warning and errors to multiple output streams.
+     */
     class Logger {
     public:
         Logger(std::string loggerName);
 
+        /**
+         * Add a new output stream to this logger.
+         */
         void addStream(std::ostream& stream);
 
+        /// Log general information.
         Logger& info(std::string msg, std::optional<LoggingInfo> info = {});
+
+        /// Log warning (non-fatal).
         Logger& warn(std::string msg, std::optional<LoggingInfo> info = {});
+
+        /// Log error (fatal).
         Logger& error(std::string msg, std::optional<LoggingInfo> info = {});
 
         static const std::string MESSAGE_END;
         static const std::string WHITE_ON_BLACK_TEXT, YELLOW_ON_BLACK_TEXT, RED_ON_BLACK_TEXT;
 
     protected:
+        /**
+         * Output logging information to all streams.
+         *
+         * @param msg Logging message to output.
+         * @param logType String indicating the logging type (e.g. WARNING, ERROR).
+         * @param escapeSequence ANSI escape sequence to apply to colours to the output.
+         * @param info Optional additional information about where the log method was called from.
+         */
         Logger& log(std::string msg, std::string logType, std::string escapeSequence, std::optional<LoggingInfo>& info);
 
+        /**
+         * Output a string through every output stream used by this logger.
+         */
         void outThroughAllStreams(std::string msg);
 
     private:
