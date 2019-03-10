6021197186af10776bd862e8e29551466b42c79e
WiredSound <maxoblack@yahoo.com>
2019-03-10 16:54:19 +0000

    Documented MOD-REG-R/M class.

--- include/common/emu/cpu/instr/modregrm.hpp
+++ include/common/emu/cpu/instr/modregrm.hpp
@@ -17,12 +17,33 @@ namespace emu::cpu::instr {
     public:
         ModRegRm(u8 modRegRmValue);
 
+        /**
+         * Fetch the three bits that comprise the R/M component of this MOD-REG-R/M byte.
+         */
         u8 getRmBits() const;
         
+        /**
+         * Fetch the three bits that make up the REG component of this MOD-REG-R/M byte.
+         */
         u8 getRegBits() const;
+
+        /**
+         * Return the appropriate register index based the value of the REG bits and specified data size.
+         *
+         * @param size The data size handled by this instruction (16-bit word or 8-bit byte).
+         * @return A variant (type-safe union) holding either a general-purpose register index or an indexing register
+         *         index.
+         */
         std::variant<GeneralIndex, IndexIndex> getRegisterIndex(DataSize size) const;
 
+        /**
+         * Fetch the pair of bits that comprise the MOD component of this MOD-REG-R/M byte.
+         */
         u8 getModBits() const;
+
+        /**
+         * Return the appropriate AddressingMode enumeration value based on the MOD component bits.
+         */
         AddressingMode getAddressingMode() const;
 
     private:
