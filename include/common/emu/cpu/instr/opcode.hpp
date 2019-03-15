29fedaf1b5ff884b74ea19f52881c6623315d0aa
WiredSound <maxoblack@yahoo.com>
2019-03-15 12:00:19 +0000

    Documented Opcode class.

--- include/common/emu/cpu/instr/opcode.hpp
+++ include/common/emu/cpu/instr/opcode.hpp
@@ -4,15 +4,29 @@
 #include "primitives.hpp"
 
 namespace emu::cpu::instr {
+    /// Indicated by the W-bit of opcode.
     enum DataSize { WORD_DATA_SIZE, BYTE_DATA_SIZE };
+
+    /// Indicated by the D-bit of opcode.
     enum RegDirection { REG_IS_SOURCE, REG_IS_DESTINATION };
 
+    /**
+     * Holder for an instruction opcode value. Has methods for fetching the direction and data size of the opcode based
+     * on the opcode's D-bit and W-bit respectively.
+     */
     class Opcode {
     public:
         Opcode(u8 opcodeValue);
 
+        /**
+         * Produces a string useful for debugging purposes (shows the opcode value in binary as well as the state of
+         * its direction and word bits).
+         */
         std::string toString() const;
 
+        /**
+         * Fetch the 6 unique bits of this opcode (removes the word and direction bits).
+         */
         u8 getUniqueValue() const;
 
         /**
