95f65c048e94c67080009b7f133484851c7917f1
WiredSound <maxoblack@yahoo.com>
2019-07-11 16:44:44 +0100

    Extended adding demo slightly.

--- demos/adding.asm
+++ demos/adding.asm
@@ -6,10 +6,17 @@
  add bx, cx
 
 ;
-;
+; Adding value at displaced address to register:
 ;
 
-add cx, [bp] ; No offset.
+add ax, [bx + 0x0F0F] ; Word offset.
+add bl, [di + 0x0F] ; Byte offset.
+
+;
+; Adding value at address (not displaced) to register:
+;
+
+add ax, [bx]
 
  ;
  ; Add immediate values to registers:
